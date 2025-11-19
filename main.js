11bfddf02c9e8c6090bcdd41b2fa4c561e8ed7c4
unknown <ezekuiel100@gmail.com>
2025-11-18 22:38:54 -0300

    Refactor parser to be more testable

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 import { evaluate } from "./evaluate.js"
 
-const code = "1 + 2 * 3"
-const exp = code.split(" ")
-let position = 0
-
-let curToken = exp[position]
-position++
-let peekToken = exp[position]
-
 let Precedence = {
     "+": 1,
     "-": 1,
@@ -15,48 +7,59 @@ let Precedence = {
     "/": 2,
 }
 
-function nextToken() {
+function parser(code) {
+    const exp = code.split(" ")
+    let position = 0
+
+    let curToken = exp[position]
     position++
-    curToken = peekToken
-    peekToken = exp[position]
-}
+    let peekToken = exp[position]
 
-function main() {
     let ast = parseExpression(0)
-    const result = evaluate(ast)
-    console.log(result)
-}
-main()
 
-function parseExpression(precedence) {
-    let leftExp = curToken
-
-    nextToken()
-
-    const peekPrecedence = getPrecedence(curToken)
-
-    while (position < exp.length && precedence < peekPrecedence) {
-        leftExp = parseRight(leftExp)
+    function nextToken() {
+        position++
+        curToken = peekToken
+        peekToken = exp[position]
     }
 
-    return leftExp
+    function parseExpression(precedence) {
+        let leftExp = curToken
+
+        nextToken()
+
+        const peekPrecedence = getPrecedence(curToken)
+
+        while (position < exp.length && precedence < peekPrecedence) {
+            leftExp = parseRight(leftExp)
+        }
+
+        return leftExp
+    }
+
+
+    function parseRight(left) {
+        let exp = { operator: curToken, left }
+
+        const precedence = getPrecedence(curToken)
+        nextToken()
+
+        exp = { ...exp, right: parseExpression(precedence) }
+
+        return exp
+    }
+
+    function getPrecedence(token) {
+        if (Precedence[token]) return Precedence[token]
+
+        return 0
+    }
+
+    return ast
 }
 
+const ast = parser("2 * 5 + 3")
+const result = evaluate(ast)
 
-function parseRight(left) {
-    let exp = { operator: curToken, left }
+console.log(result)
 
-    const precedence = getPrecedence(curToken)
-    nextToken()
-
-    exp = { ...exp, right: parseExpression(precedence) }
-
-    return exp
-}
-
-
-function getPrecedence(token) {
-    if (Precedence[token]) return Precedence[token]
-
-    return 0
-}
